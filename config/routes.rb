27f65b5bece697e3243683d6c5d5c802cc118785
Kylian Janssens <kylianjanssens@hotmail.com>
2023-11-14 20:15:26 +0100

    fixed bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root 'projects#index'
+  resources :projects
+  get '/about', to: 'pages#about', as: 'about'
+  get '/shop', to: 'pages#shop', as: 'shop'
+  get '/contact', to: 'pages#contact', as: 'contact'
 end
