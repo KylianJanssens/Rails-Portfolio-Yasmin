0d6a1bf0cd7b0556217b9f36dc9dc555b3031e25
Kylian Janssens <kylianjanssens@hotmail.com>
2023-11-28 20:22:49 +0100

    fixed the sign out link for devise - made it not possible to create more users with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, #//:registerable,//
          :recoverable, :rememberable, :validatable
 end
+
+# I have toggled registerable so others cannot register if it works just delete all togheter
+# # Assuming you have a User model
+# user = User.new(email: 'admin@example.com', password: 'your_password', password_confirmation: 'your_password')
+
+# # Save the user
+# user.save
